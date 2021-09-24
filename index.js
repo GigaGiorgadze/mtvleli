cc75adc25eac8d0a154421fff569d79a675b27f0
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2021-09-24 17:17:28 +0400

    adding page navigation

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const frame = document.querySelector('iframe')
+const navLinks = document.querySelectorAll('.nav-link')
+
+console.log(navLinks)
+
+const linkSwitcher = (e) => {
+    if(e.target.id == 'party'){
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQpdYb8AVRu9YFtxzRmZDwVdjSHL0-KcG_wLbs3xKAzcQjUeAxedUn5j1J6ehj6rA/pubhtml?widget=true&amp;headers=false'
+    }else if(e.target.id == 'major'){
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRmXxZBbLlBTVMV96UQtVTpkXJUdyfOq7rdqq4Hb8QNZoKUER1GDPi3IVTH5xr4BA/pubhtml?widget=true&amp;headers=false'
+    }else if(e.target.id == 'mayor'){
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQecaMuuCqJg9B9T-cQVouusOw_7NZrb_0AKpU_s2KAXSrCcoDOz9yegmOzqn2G-A/pubhtml?widget=true&amp;headers=false'
+    }
+}
+
+navLinks.forEach(navLink => navLink.addEventListener('click', e => (linkSwitcher(e))))
+
+
