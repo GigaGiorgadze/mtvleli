a62907c2e9dc991bac31a33569a36b99d75cc5e9
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2021-09-26 11:00:59 +0400

    changed way website works

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const frame = document.querySelector('iframe')
-const navLinks = document.querySelectorAll('.nav-link')
-
-console.log(navLinks)
-
-const linkSwitcher = (e) => {
-    if(e.target.id == 'party'){
-        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQpdYb8AVRu9YFtxzRmZDwVdjSHL0-KcG_wLbs3xKAzcQjUeAxedUn5j1J6ehj6rA/pubhtml?widget=true&amp;headers=false'
-    }else if(e.target.id == 'major'){
-        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRmXxZBbLlBTVMV96UQtVTpkXJUdyfOq7rdqq4Hb8QNZoKUER1GDPi3IVTH5xr4BA/pubhtml?widget=true&amp;headers=false'
-    }else if(e.target.id == 'mayor'){
-        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQecaMuuCqJg9B9T-cQVouusOw_7NZrb_0AKpU_s2KAXSrCcoDOz9yegmOzqn2G-A/pubhtml?widget=true&amp;headers=false'
-    }
-}
-
-navLinks.forEach(navLink => navLink.addEventListener('click', e => (linkSwitcher(e))))
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSUxTGbr4rwhlWdcnpY46a66bZEvBtRfKBfrVDKOQBs-vbpE_xP5MoJzyvguBq_eTH61tqspY0Ip3C2/pubhtml?widget=true&amp;headers=false'
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_jO1cjC9lSqkDCw42YYSTuXWnLnXbz3mqiPJXCbI011wrRrrxLFoHaZoBDF4Kg3RPkG67vOqtLbmf/pubhtml?widget=true&amp;headers=false'
+        frame.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-UWzgeS6_XbEs-vWAR-vEugCk6JgKAN7ULmaQ0FHawhOHz374DdLa9YxAtKLhcIxtSJynH8t4m5jS/pubhtml?widget=true&amp;headers=false'
 
 
